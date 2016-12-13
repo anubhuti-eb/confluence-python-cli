771c6827eae3c878bed752f6aa903b8a6e06002f
Anubhuti Chaudhary <anubhuti@eventbrite.com>
2016-12-13 13:58:30 -0800

    Remove package with underscores

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='confluence-python-cli',
     version='1.0',
-    packages=['confluence-python-cli',],
+    packages=['confluence_python_cli',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
 )
