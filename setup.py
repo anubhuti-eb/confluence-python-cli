ac60dd718d87e6b6dc5e1e94ab0112af27918f15
Anubhuti Chaudhary <anubhuti@eventbrite.com>
2016-12-13 13:46:01 -0800

    Create proper python package for confluence python cli

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='confluence-python-cli',
+    version='1.0',
+    packages=['confluence-python-cli',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
