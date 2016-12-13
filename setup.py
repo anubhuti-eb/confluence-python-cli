3776154154ee84b52104058762d2eab61d6d9fdd
Anubhuti Chaudhary <anubhuti@eventbrite.com>
2016-12-13 14:01:04 -0800

    Rename package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(
-    name='confluence-python-cli',
+    name='confluence_python_cli',
     version='1.0',
     packages=['confluence_python_cli',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
