e14e18c0ae2b55798698c2514434b0356d8cfeab
Anubhuti Chaudhary <anubhuti@eventbrite.com>
2016-12-14 11:26:53 -0800

    Changed the license to GPLv3

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='confluence_python_cli',
     version='1.0',
     packages=['confluence_python_cli',],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    license='GPLv3',
     long_description=open('README.md').read(),
 )
